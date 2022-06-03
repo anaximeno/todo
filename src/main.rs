a44f84fba36241d87507a3aaca1c850af12fe7d5
anaximeno <anaximenobrito@gmail.com>
2022-06-03 18:17:49 -0100

    docs: Update description of some functions

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ struct Database {
 }
 
 impl From<&str> for Database {
-    /// Get's a new instance of the struct
+    /// Initializes the database from a given path
     fn from(path: &str) -> Self {
         let path = String::from(path);
         let conn = Connection::open(&path).unwrap();
@@ -16,6 +16,7 @@ impl From<&str> for Database {
 }
 
 impl Database {
+    /// Initializes the database from the memory
     fn new() -> Self {
         let path = String::from(":memory:");
         let conn = Connection::open(&path).unwrap();
