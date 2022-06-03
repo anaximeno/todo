3e508ea59d68f870274658d607241087e1039155
anaximeno <anaximenobrito@gmail.com>
2022-06-03 12:20:29 -0100

    feat: Add methods to reference Database fields

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use sqlite::Connection;
 
+/// Database handler for the aplication
 struct Database {
     path: String,
     conn: Connection
 }
 
 impl Database {
+    /// Get's a new instance of the struct
     fn new(path: &str) -> Self {
         let path = String::from(path);
         let conn = Connection::open(&path).unwrap();
         Self{path, conn}
     }
+
+    /// References the path of the db
+    fn path(&self) -> &String {
+        &self.path
+    }
+
+    /// References the connection to the database
+    fn connection(&self) -> &Connection {
+        &self.conn
+    }
 }
 
 fn main() {
     let db = Database::new(":memory:");
+    println!("Connected db at '{}'", db.path());
 }
