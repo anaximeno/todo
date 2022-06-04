dad368fa38f599e74674c4037a0c59a4e4683958
anaximeno <anaximenobrito@gmail.com>
2022-06-03 23:50:43 -0100

    feat: Add task and status for tasks
    * Added enum status
    * implemented the From trait in status
    * Added that struct Task

--- todo_lib/src/lib.rs
+++ todo_lib/src/lib.rs
@@ -6,3 +6,34 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+#[derive(Debug, PartialEq)]
+/// Used to define the current status of
+/// a task. The done pattern should be used to
+/// store the date that the task was set as done.
+pub enum Status {
+    Done(String),
+    Todo,
+}
+
+impl From<&str> for Status {
+    fn from(date_completed: &str) -> Self {
+        Status::Done(String::from(date_completed))
+    }
+}
+
+impl From<String> for Status {
+    fn from(date_completed: String) -> Self {
+        Status::Done(date_completed)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+/// A task is something the user
+/// wants to do.
+pub struct Task {
+    id: u32,
+    title: String,
+    date_added: Option<String>,
+    status: Status
+}
