75c69f5276e5d79bd95e0390321bedbd5535333e
anaximeno <anaximenobrito@gmail.com>
2022-06-03 23:54:32 -0100

    feat: Implemented the Task struct

--- todo_lib/src/lib.rs
+++ todo_lib/src/lib.rs
@@ -47,3 +47,56 @@ pub struct Todo {
     description:  Option<String>,
     tasklist:     Vec<Task>,
 }
+
+impl Task {
+    /// Creates a new task by determining the id and the title,
+    /// others fields are set to default.
+    pub fn new(id: u32, title: String) -> Self {
+        Self{id, title, date_added: None, status: Status::Todo}
+    }
+
+    /// Creates a new task with description.
+    pub fn with_date(id: u32, title: String, date_added: String) -> Self {
+        Self{id, title, date_added: Some(date_added), status: Status::Todo}
+    }
+
+    /// Creates a new task with a pre-defined status.
+    pub fn with_status(id: u32, title: String, date_added: String, status: Status) -> Self {
+        Self{id, title, date_added: Some(date_added), status}
+    }
+
+    /// Sets the task status to a new one.
+    pub fn set_status(&mut self, status: Status) {
+        self.status = status;
+    }
+
+    /// Sets a new date_added to the task.
+    pub fn set_date_added(&mut self, date: String) {
+        self.date_added = Some(date);
+    }
+
+    /// Sets a new title to the task.
+    pub fn set_title(&mut self, title: String) {
+        self.title = title;
+    }
+
+    /// Returns the task's id.
+    pub fn id(&self) -> &u32 {
+        &self.id
+    }
+    
+    /// Returns a reference to the date the task was added.
+    pub fn date_added(&self) -> &Option<String> {
+        &self.date_added
+    }
+
+    /// Returns a reference to the task's status.
+    pub fn status(&self) -> &Status {
+        &self.status
+    }
+
+    /// Returns a reference to the task's title.
+    pub fn title(&self) -> &String {
+        &self.title
+    }
+}
