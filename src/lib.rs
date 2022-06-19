62c4793a246fe924280ef9f24f60745eed2eb0a0
anaximeno <anaximenobrito@gmail.com>
2022-06-19 22:58:25 -0100

    refactor: Moved db logic to a new mod named back

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use super::core::*;
+    use super::back::*;
 
     #[test]
     fn test_task_creation() {
@@ -43,6 +44,20 @@ mod tests {
             Task::new(23, "Test this task")
         ).unwrap();
         assert_ne!(list.get_task(23), None);
+    }
+
+    #[test]
+    fn test_add_and_get_a_todo() {
+        let mut art = Artisan::new(":memory:");
+        art.add_todo("test", "this a test todo!").unwrap();
+        assert_ne!(art.get_todo("test"), None);
+    }
+
+    #[test]
+    fn test_add_and_get_a_task_by_id() {
+        let mut art = Artisan::new(":memory:");
+        art.add_task("check", "test-todo").unwrap();
+        assert_eq!(art.get_task(1).unwrap().task(), "check");
     }
 }
 
@@ -249,6 +264,177 @@ pub mod core {
         pub fn get_task_mut(&mut self, taskid: IdIntType) -> Option<&mut Task> {
             let index = self.get_task_index(taskid);
             if let Some(idx) = index { self.tasks.get_mut(idx) } else { None }
+        }
+    }
+}
+
+pub mod back {
+    #![allow(unused)]
+
+    use super::core::*;
+
+    use sqlite::{
+        self,
+        Connection
+    };
+    
+    /// Database handler for the aplication
+    struct Database {
+        path: String,
+        conn: Connection
+    }
+
+    /// Responsible for interations
+    /// with the Database
+    pub struct Artisan {
+        db: Database
+    }
+
+    impl From<&str> for Database {
+        /// Initializes the database from a given path
+        fn from(path: &str) -> Self {
+            let path = String::from(path);
+            let conn = Connection::open(&path).unwrap();
+            Self{path, conn}
+        }
+    }
+    
+    impl Database {
+        /// References the path of the db
+        fn path(&self) -> &String {
+            &self.path
+        }
+    
+        /// References the connection to the database
+        fn connection(&self) -> &Connection {
+            &self.conn
+        }
+    
+        /// Executes a query command into the database
+        fn exec(&self, statement: &str) -> Result<(), sqlite::Error> {
+            self.connection().execute(statement)
+        }
+    
+        /// Executes a select query and returns a cursor
+        fn select_query(&mut self, query: &str) -> Result<sqlite::Cursor, &str> {
+            if let Ok(res) = self.conn.prepare(query) {
+                Ok(res.into_cursor())
+            } else {
+                Err("Error executing the query!")
+            }
+        }
+    }
+    
+    impl Artisan {
+        /// Creates a new artisan
+        pub fn new(db_path: &str) -> Self {
+            let this = Self{db: Database::from(db_path)};
+            this.init_db().expect("Error Initializing the DB!");
+            this
+        }
+    
+        /// Returns a reference to the path of the db
+        pub fn get_db_path(&self) -> &String {
+            self.db.path()
+        }
+    
+        /// Initializes the sqlite database with the default relations,
+        /// if not already created.
+        fn init_db(&self) -> Result<(), sqlite::Error> {
+            self.db.exec("
+            CREATE TABLE IF NOT EXISTS Todos(
+                todo_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                description TEXT NOT NULL
+            );") ? ;
+            self.db.exec("
+            CREATE TABLE IF NOT EXISTS Tasks(
+                task_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                task TEXT NOT NULL,
+                todo_id INTEGER NOT NULL,
+                date_added DATETIME NOT NULL DEFAULT CURRENT_DATE,
+                date_completed DATETIME,
+                FOREIGN KEY (todo_id) REFERENCES Todos(todo_id)
+            );") ? ;
+            Ok(())
+        }
+    
+        /// Add a new todo to the database
+        pub fn add_todo(&self, name: &str, description: &str) -> Result<(), sqlite::Error> {
+            self.db.exec(&format!("INSERT INTO Todos(name, description) VALUES ('{}', '{}')", name, description)) ? ;
+            Ok(())
+        }
+    
+        /// Queries and returns if found a todo from the
+        /// database using the name.
+        pub fn get_todo(&mut self, name: &str) -> Option<Todo> {
+            self.db
+            .select_query(&format!("SELECT todo_id, description FROM Todos WHERE name = '{}'", name))
+            .expect(&format!("Could not query the todo: '{}'", name))
+            .next()
+            .unwrap()
+            .map(|todo| {
+                let id = todo[0].as_integer().unwrap() as IdIntType;
+                let description = todo[1].as_string().unwrap();
+                Todo::with_description(id, name, description)
+            })
+        }
+    
+        pub fn get_task_id(&mut self, task: &str) -> Option<IdIntType> {
+            self.db
+            .select_query(&format!("SELECT task_id FROM Tasks WHERE task = '{}'", task))
+            .expect("Could not query for the task's id into the database!")
+            .next()
+            .unwrap()
+            .map(|res| {
+                let id = res[0].as_integer().unwrap();
+                id as IdIntType
+            })
+        }
+    
+        fn insert_task_into_the_db(&mut self, task: &str, todo_id: IdIntType) -> Result<(), sqlite::Error> {
+            self.db.exec(&format!("INSERT INTO Tasks(task, todo_id) VALUES('{}', {})", task, todo_id))
+        }
+    
+        /// Add a new task to the database
+        pub fn add_task(&mut self, task: &str, todo_name: &str) -> Result<(), &str> {
+            if let Some(todo) = self.get_todo(todo_name) {
+                let task_id = self.get_task_id(task);
+    
+                if let Some(id) = task_id {
+                    return Err("Task added more than one time to the todo!");
+                }
+    
+                if let Err(_) = self.insert_task_into_the_db(task, *todo.id()) {
+                    return Err("Error inserting the task into the Database!");
+                }
+            } else {
+                self.add_todo(todo_name, "").unwrap();
+                self.add_task(task, todo_name) ? ;
+            }
+            Ok(())
+        }
+    
+        /// Returns a task of the database if found
+        pub fn get_task(&mut self, task_id: IdIntType) -> Option<Task> {
+            self.db
+            .select_query(&format!("SELECT task, date_added, date_completed FROM Tasks WHERE task_id = {}", task_id))
+            .unwrap()
+            .next()
+            .unwrap()
+            .map(|res| {
+                let task = res[0].as_string().unwrap();
+                let date_added = res[1].as_string().unwrap();
+                let status = match res[2].as_string() {
+                    Some(date) => Status::from(date),
+                    None => Status::Todo };
+                Task::with_status(
+                    task_id,
+                    task,
+                    date_added,
+                    status
+                )
+            })
         }
     }
 }
