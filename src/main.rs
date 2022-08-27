fa2e3225703bb08bb9b4cb70d49661d147f96aa0
anaximeno <anaximenobrito@gmail.com>
2022-08-27 21:29:36 -0100

    refactor: rebasing main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use todo::back::*;
+use todo::prelude::*;
 
-use sqlite;
 
 use clap::{
     Command,
@@ -8,24 +7,10 @@ use clap::{
     Arg,
 };
 
-macro_rules! printerr {
-    ($msg:tt) => {
-        println!("Todo: Err: {}", $msg)
-    };
-
-    ($msg:tt, $exit_code:expr) => {
-        println!("ToDo: Err: {}", $msg);
-        // TODO: Check if $exit_code is an
-        // integer type.
-        std::process::exit($exit_code);
-    };
-}
-
 /// Represents the todo application
 struct App {
     name: String,
-    version: String,
-    dao: TodoDatabaseDAO,
+    version: String
 }
 
 fn parse_args(app_name: &str, app_version: &str) -> ArgMatches {
@@ -48,14 +33,14 @@ fn parse_args(app_name: &str, app_version: &str) -> ArgMatches {
                             .help("The task to be added to the todo list")
                             .takes_value(true)
                             .multiple_occurrences(true)
-                            .multiple_values(true)                    
+                            .multiple_values(true)
                     )
                     .arg(
                         Arg::new("description")
                             .short('d')
                             .long("desc")
                             .help("The Description of the todo")
-                            .takes_value(true)   
+                            .takes_value(true)
                     )
             ).subcommand(
                 Command::new("done")
@@ -65,11 +50,7 @@ fn parse_args(app_name: &str, app_version: &str) -> ArgMatches {
 impl App {
     /// Used to create an app
     fn new(name: &str, version: &str) -> Self {
-        Self {
-            dao: TodoDatabaseDAO::new(":memory:"),
-            name: String::from(name),
-            version: String::from(version)
-        }
+        Self {name: String::from(name), version: String::from(version)}
     }
 
     /// References the name of this app
@@ -82,47 +63,21 @@ impl App {
         &self.version
     }
 
-    fn add_todo(&mut self, name: &str, desc: Option<&str>, tasks: Option<Vec<&str>>) -> Result<(), sqlite::Error> {
-        self.dao.add_todo(name, desc) ? ;
-        if let Some(todo) = self.dao.get_todo_by_name(name) {
-            if let Some(tasks) = tasks {
-                for task in tasks {
-                    self.dao.add_task(task, todo.name()) ? ;
-                }
-            }
-        }
-        Ok(())
-    }
-
     /// Run main routine
     fn run(&mut self) {
         let args = parse_args(self.name(), self.version());
 
         match args.subcommand() {
             Some(("add", add_matches)) => {
-                let name = add_matches.get_one::<String>("name").unwrap();
+                let name = add_matches.get_one::<String>("name")
+                                                       .map(|s| String::from(s)).unwrap();
 
-                let tasks = match add_matches.get_many::<String>("tasks") {
-                    Some(tasks) => Some(tasks.map(|s| s.as_str()).collect::<Vec<_>>()),
-                    None => None
-                };
+                let description = add_matches.get_one::<String>("description")
+                                                             .map(|s| String::from(s));
 
-                let description = match add_matches.get_one::<String>("description") {
-                    Some(desc) => Some(desc.as_str()),
-                    None => None
-                };
+                let todo = Todo::add(name, description).unwrap();
 
-                if let Err(e) = self.add_todo(name, description, tasks) {
-                    let msg = match e.message {
-                        Some(msg) => msg,
-                        None => format!("Could not add todo '{}' to the database!", name)
-                    };
-                    printerr!(msg, 1);
-                }
-
-                if let Some(todo) = self.dao.get_todo_with_all_tasks(1) {
-                    println!("{:#?}", todo);
-                }
+                println!("Todo added: {:#?}", todo);
             },
             _ => ()
         }
@@ -131,6 +86,10 @@ impl App {
 }
 
 fn main() {
+    // XXX
+    Todo::init_table().unwrap();
+    Task::init_table().unwrap();
+
     let mut app = App::new("TodoApp", "2.2.0");
     app.run();
 }
